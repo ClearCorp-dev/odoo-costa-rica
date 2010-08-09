849713e1e5cca9fcf3086c49592b23d49e9c1ac7
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2010-08-08 21:46:59 -0600

    [FIX] l10n_cr_base.py: field_get function call

--- l10n_cr_base/l10n_cr_base.py
+++ l10n_cr_base/l10n_cr_base.py
@@ -33,7 +33,7 @@
 ##############################################################################
 
 from osv import osv,fields
-from base.res.partner import partner
+from base.res.partner.partner import _lang_get
 
 class res_partner_function(osv.osv):
     '''
