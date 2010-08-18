caaf5de001fbd53e22b1804d427138a004216f3d
Susana García <sgarcia@clearcorp.co.cr>
2010-08-17 22:41:50 -0600

    __terp__.py changed

---
+++ l10n_cr_base/l10n_cr_base_data.sql
@@ -0,0 +1,15 @@
+/*		res.lang
+		Update fields 
+]*/
+
+update res_users		
+	set name = 'Spanish / Español',
+		active = 'True',
+		translatable = 'True',
+		grouping = '[3]',
+		direction = 'ltr',
+		date_format = '%d/%m/%Y',
+		time_format = '%I:%M:%S %p',
+		decimal_point = ',',
+		thousands_sep = '.'
+	where code = 'es_ES'
