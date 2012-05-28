d113a778983b2d4f1268386f5e256e981869bfc1
Armando Soto <armando.soto@clearcorp.co.cr>
2012-05-28 11:44:08 -0600

    [ADD] Added l10n_cr_account_voucher [FIX]   Reference field changed to required

---
+++ l10n_cr_account_voucher/l10n_cr_account_voucher.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from lxml import etree
+
+import netsvc
+from osv import osv, fields
+import decimal_precision as dp
+from tools.translate import _
+
+class account_voucher(osv.osv):
+    _name = 'account.voucher' 
+    _inherit = 'account.voucher'
+    _columns = {
+        'reference': fields.char('Ref #', size=64, readonly=True, required=True, states={'draft':[('readonly',False)]}, help="Transaction reference number."),
+    }
+
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
