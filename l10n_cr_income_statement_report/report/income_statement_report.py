344e36c65fbfc66997cab197f603315c75435c99
Carlos Vásquez <carlos.vasquez@clearcorp.co.cr>
2012-11-07 20:43:28 -0600

    [FIX] testing report

--- l10n_cr_income_statement_report/report/income_statement_report.py
+++ l10n_cr_income_statement_report/report/income_statement_report.py
@@ -108,9 +108,10 @@ class IncomeStatementReport(TrialBalanceWebkit):
         #build accounts list
         income_accounts = [category_account_ids['income']]
         income_accounts += (library_obj.get_account_child_ids(cr, uid, category_account_ids['income']))
-        print income_accounts
+        print 'income_accounts', income_accounts
         expense_accounts = [category_account_ids['expense']]
         expense_accounts += (library_obj.get_account_child_ids(cr, uid, category_account_ids['expense']))
+        print 'expense_accounts', expense_accounts
         
         #build account_ids list
         income_account_ids = []
