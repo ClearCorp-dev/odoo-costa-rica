60ba5dd84c820220b41e0c14cd6f5ca3b95a66e4
Carlos Navarro <carlos.navarro@imesasistemas.com>
2015-06-17 10:17:20 -0600

    [ADD]Add button for send for mail, the payslip report of employee

--- l10n_cr_hr_payroll/l10n_cr_hr_payroll.py
+++ l10n_cr_hr_payroll/l10n_cr_hr_payroll.py
@@ -68,8 +68,8 @@ class hrPaysliprun(osv.Model):
 
 
 class hrPayslipinherit(osv.Model):
-
-    _inherit = 'hr.payslip'
+    _name='hr.payslip'
+    _inherit = ['mail.thread','hr.payslip']
 
     #Get total payment per month
     def get_qty_previous_payment(self, cr, uid, employee, actual_payslip, context=None):
@@ -151,3 +151,37 @@ class hrPayslipinherit(osv.Model):
             month_date_next = next_date.month
             payments += 1
         return payments
+    
+    def action_payslip_send(self, cr, uid, ids, context=None):
+        '''
+        This function opens a window to compose an email, with the payslip template message loaded by default
+        '''
+        assert len(ids) == 1, 'This option should only be used for a single id at a time.'
+        ir_model_data = self.pool.get('ir.model.data')
+        try:
+            template_id = ir_model_data.get_object_reference(cr, uid, 'l10n_cr_hr_payroll', 'email_template_payslip')[1]
+        except ValueError:
+            template_id = False
+        try:
+            compose_form_id = ir_model_data.get_object_reference(cr, uid, 'mail', 'email_compose_message_wizard_form')[1]
+        except ValueError:
+            compose_form_id = False 
+        ctx = dict()
+        ctx.update({
+            'default_model': 'hr.payslip',
+            'default_res_id': ids[0],
+            'default_use_template': bool(template_id),
+            'default_template_id': template_id,
+            'default_composition_mode': 'comment',
+            'mark_so_as_sent': True
+        })
+        return {
+            'type': 'ir.actions.act_window',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'mail.compose.message',
+            'views': [(compose_form_id, 'form')],
+            'view_id': compose_form_id,
+            'target': 'new',
+            'context': ctx,
+        }
